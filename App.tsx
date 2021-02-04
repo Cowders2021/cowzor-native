f12316150b80a31d07005a427b6217187f55b50a
David Øvrelid <david.ovrelid@felleskjopet.no>
2021-02-04 17:02:00 +0100

    Created toast to handle successs and error messages

--- App.tsx
+++ App.tsx
@@ -2,20 +2,26 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { NativeRouter, Route } from "react-router-native";
 import Header from './components/Header';
+import FkaToast from './components/Toast';
+import ToastStore from './contexts/ToastContext';
 import WeatherStore from './contexts/WeatherContext';
 import Home from "./pages/Home";
 import Login from './pages/Login';
 
 export default function App() {
+
   return (
     <>
       <StatusBar style="auto" />
       <NativeRouter>
-        <Header />
-        <WeatherStore>
-          <Route exact path="/" component={Login} />
-          <Route path="/home" component={Home} />
-        </WeatherStore>
+        <ToastStore>
+          <Header />
+          <WeatherStore>
+            <Route exact path="/" component={Login} />
+            <Route path="/home" component={Home} />
+          </WeatherStore>
+          <FkaToast />
+        </ToastStore>
       </NativeRouter>
     </>
   );
--- styles/Colors.ts
+++ styles/Colors.ts
@@ -3,3 +3,8 @@ export const FKA_PRIMARY_FG = '#ffffff';
 
 export const FKA_YELLOW = '#ffe500';
 export const FKA_YELLOW_FG = '#000000'
+
+export const FKA_BASIC = '#FFF';
+export const FKA_BASIC_DARK = '#000'
+
+export const FKA_DANGER = 'red';
